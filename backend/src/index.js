48d997edf2dac427d0b24e3517213da245b5edac
AugustoMarcelo <mrclgst10@gmail.com>
2020-01-14 16:48:41 -0300

    Adicionada flag de método depreciado

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,9 +4,11 @@ const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb://192.168.99.100:27017/aircnc', {
+mongoose.connect('mongodb://192.168.99.100:27017/findev', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 app.use(express.json());
