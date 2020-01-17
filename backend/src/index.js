49799da244767d69bb42e2bf104ef8163b7616f5
AugustoMarcelo <mrclgst10@gmail.com>
2020-01-17 20:51:00 -0300

    Protocolo HTTP abstraído; acrescentado protocolo websocket

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
+
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb://192.168.99.100:27017/findev', {
   useNewUrlParser: true,
@@ -16,4 +22,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
